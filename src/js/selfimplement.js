c1d0d1a2e966d403ac1aa5728c98de108a829e67
Jack <mengbinhao2018@gmail.com>
2018-12-13 09:33:10 +0800

    minor change

--- src/js/selfimplement.js
+++ src/js/selfimplement.js
@@ -228,6 +228,48 @@ let deepClone = (target, origin) => {
 }
 
 
+let copyDeepClone = function (obj) {
+    var copy;
+
+    // Handle the 3 simple types, and null or undefined
+    if (null == obj || "object" != typeof obj) return obj;
+
+    // Handle Date
+    if (obj instanceof Date) {
+      copy = new Date();
+      copy.setTime(obj.getTime());
+      return copy;
+    }
+
+    // Handle Array
+    if (obj instanceof Array) {
+      copy = [];
+      for (var i = 0, len = obj.length; i < len; i++) {
+          copy[i] = deepClone(obj[i]);
+      }
+      return copy;
+    }
+
+    // Handle Function
+    if (obj instanceof Function) {
+        copy = function() {
+            return obj.apply(this, arguments);
+        }
+        return copy;
+
+        // Handle Object
+        if (obj instanceof Object) {
+            copy = {};
+            for (var attr in obj) {
+                if (obj.hasOwnProperty(attr)) copy[attr] = deepClone(obj[attr]);
+            }
+            return copy;
+        }
+
+        throw new Error("Unable to copy obj as type isn't supported " + obj.constructor.name);
+    }
+}
+
 /**
  * 1. 新生成了一个对象
  * 2. 链接到原型
@@ -337,8 +379,8 @@ let curryFormalParameter = function (fn, args) {
 //String
 String.prototype.MyReverse = function () {
     return Array.prototype.reverse.apply(this.split('')).join('');
-};
+}
 
 String.prototype.MyTrim = function () {
     return this.replace(/^\s+|\s+$/g, '');
-};
+}
