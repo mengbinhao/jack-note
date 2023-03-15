ebf78bb5e1000073de78e1b89d569a7666ba0401
JackM <mengbinhao2018@gmail.com>
2023-03-15 11:39:55 +0800

    update

--- docs/frontend/vue/test.js
+++
@@ -1,8 +0,0 @@
-document.styleSheets[0].insertRule('.box {height: 100px}', 0)
-
-// 或者插入新样式时操作
-var styleEl = document.createElement('style'),
-	styleSheet = styleEl.sheet
-
-styleSheet.insertRule('.box {height: 100px}', 0)
-styleSheet.document.head.appendChild(styleEl)
